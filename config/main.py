d03ba4faca530bfb1898afee8cc5385b548d0033
Vadym Hlushko <62022266+vadymhlushko-mlnx@users.noreply.github.com>
2021-09-13 11:01:45 +0300

    [202012] [portstat, intfstat] added rates and utilization  (#1812)
    #### What I did
    
    Backport the [pull/1750](https://github.com/Azure/sonic-utilities/pull/1750) to `202012` branch
    
    Depends on [sonic-swss-common/pull/330](https://github.com/Azure/sonic-swss-common/pull/330)
    
    According to [HLD](https://github.com/Azure/SONiC/blob/master/doc/rates-and-utilization/Rates_and_utilization_HLD.md) added calculation of rates and utilization columns to the `portstat` and `intfstat` scripts output
    
    #### How I did it
    
    Modified the `portstat` and `intfstat` scripts
    
    #### How to verify it
    
    Added UT

--- config/main.py
+++ config/main.py
@@ -4626,5 +4626,33 @@ def delete(ctx):
     sflow_tbl['global'].pop('agent_id')
     config_db.set_entry('SFLOW', 'global', sflow_tbl['global'])
 
+#
+# 'rate' group ('config rate ...')
+#
+
+@config.group()
+def rate():
+    """Set port rates configuration."""
+    pass
+
+
+@rate.command()
+@click.argument('interval', metavar='<interval>', type=click.IntRange(min=1, max=1000), required=True)
+@click.argument('rates_type', type=click.Choice(['all', 'port', 'rif']), default='all')
+def smoothing_interval(interval, rates_type):
+    """Set rates smoothing interval """
+    counters_db = swsssdk.SonicV2Connector()
+    counters_db.connect('COUNTERS_DB')
+
+    alpha = 2.0/(interval + 1)
+
+    if rates_type in ['port', 'all']:
+        counters_db.set('COUNTERS_DB', 'RATES:PORT', 'PORT_SMOOTH_INTERVAL', interval)
+        counters_db.set('COUNTERS_DB', 'RATES:PORT', 'PORT_ALPHA', alpha)
+    if rates_type in ['rif', 'all']:
+        counters_db.set('COUNTERS_DB', 'RATES:RIF', 'RIF_SMOOTH_INTERVAL', interval)
+        counters_db.set('COUNTERS_DB', 'RATES:RIF', 'RIF_ALPHA', alpha)
+
+
 if __name__ == '__main__':
     config()
