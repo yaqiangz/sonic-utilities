96d658c2e3e2d717b6d62b05a42abe5b6b72f203
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2021-09-13 14:48:42 +0800

    [202012][sonic installer] Add swap setup support (#1815)
    What I did
    Port PR #1787 to 202012 branch

--- sonic_installer/common.py
+++ sonic_installer/common.py
@@ -15,6 +15,7 @@ IMAGE_PREFIX = 'SONiC-OS-'
 IMAGE_DIR_PREFIX = 'image-'
 ROOTFS_NAME = 'fs.squashfs'
 
+
 # Run bash command and print output to stdout
 def run_command(command):
     click.echo(click.style("Command: ", fg='cyan') + click.style(command, fg='green'))
@@ -27,14 +28,15 @@ def run_command(command):
     if proc.returncode != 0:
         sys.exit(proc.returncode)
 
+
 # Run bash command and return output, raise if it fails
-def run_command_or_raise(argv):
+def run_command_or_raise(argv, raise_exception=True):
     click.echo(click.style("Command: ", fg='cyan') + click.style(' '.join(argv), fg='green'))
 
     proc = subprocess.Popen(argv, text=True, stdout=subprocess.PIPE)
     out, _ = proc.communicate()
 
-    if proc.returncode != 0:
+    if proc.returncode != 0 and raise_exception:
         raise SonicRuntimeException("Failed to run command '{0}'".format(argv))
 
     return out.rstrip("\n")
