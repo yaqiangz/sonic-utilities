6189a1471c1224eb7df52f01f971a2a489ba2e9a
Yaqiang Zhu <1512099831@qq.com>
2023-02-09 10:55:56 +0800

    Restore handle_restart_dhcp_relay_service

--- utilities_common/dhcp_relay_util.py
+++ utilities_common/dhcp_relay_util.py
@@ -12,13 +12,9 @@ def restart_dhcp_relay_service():
     clicommon.run_command("systemctl start dhcp_relay", display_cmd=False)
 
 
-def handle_restart_dhcp_relay_service(ignore_system_exit_error=False):
+def handle_restart_dhcp_relay_service():
     try:
         restart_dhcp_relay_service()
     except SystemExit as e:
         ctx = click.get_current_context()
-
-        if ignore_system_exit_error:
-            ctx.exit(0)
-
         ctx.fail("Restart service dhcp_relay failed with error {}".format(e))
